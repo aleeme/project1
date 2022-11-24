0be2067b7e8784c70759b07b0d34030128958862
alex.mm <alex.9507@kw.ac.kr>
2022-11-25 02:51:51 +0900

    update

--- stockapp/config/DB.js
+++ stockapp/config/DB.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     user: process.env.DB_USER || '',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB || '',
-    port: process.env.DB_PORT || '..',
+    port: process.env.DB_PORT || '',
 });
 connection.connect((err) => {
     console.log("try connect to DB...");
